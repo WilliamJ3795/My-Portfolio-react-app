2615b92f975fdbe8548d46982697f1a4dfaeecd8
William James <Williamj3795@gmail.com>
2023-01-16 16:34:49 -0500

    added svg and changed colors

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
     <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start 
                        md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
           Hi! My name is William,
           <br className="hidden lg:inline-block" /> I love to build amazing
           apps.
@@ -19,7 +19,7 @@ export default function About() {
         <div className="flex justify-center">
           <a
             href="#contact"
-            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+            className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">
             Work With Me
           </a>
           <a
@@ -33,7 +33,8 @@ export default function About() {
         <img
           className="object-cover object-center rounded"
           alt="hero"
-          src="./coding.svg"
+          src=
+          "./coding.svg"
         />
       </div>
     </div>
