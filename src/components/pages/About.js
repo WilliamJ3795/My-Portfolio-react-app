e6a4214144d03d6510cb109070164edacd8b00d3
William James <Williamj3795@gmail.com>
2023-01-17 23:08:17 -0500

    changed color of header, size of header, contact text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,26 +8,24 @@ export default function About() {
                        md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
           Hi! My name is William,
-          <br className="hidden lg:inline-block" /> I love to build amazing
-          apps.
+          <br className="hidden lg:inline-block" /> I love web development!
         </h1>
         <p className="mb-8 leading-relaxed">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
           laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
           Laborum, voluptas natus?
         </p>
-        <div className="flex justify-center">
+        {/* <div className="flex justify-center">
           <a
             href="#contact"
             className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">
             Work With Me
           </a>
-          <a
-            href="#projects"
+          <Link to="Projects"
             className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
             See My Past Work
-          </a>
-        </div>
+          </Link>
+        </div> */}
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
         <img
