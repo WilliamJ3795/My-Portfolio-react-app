117980355d72f4202e6956821602e01be8b0c4a9
William James <Williamj3795@gmail.com>
2023-01-18 22:51:25 -0500

    changed page names for conditional rendering

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -8,21 +8,27 @@ import Testimonials from './pages/Testimonials';
 
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
-    return <Contact />;
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    if (currentPage === 'Skills') {
+      return <Skills />;
+    }
+    if (currentPage === 'Testimonials') {
+      return <Testimonials />;
+     
   };
+}
 
   const handlePageChange = (page) => setCurrentPage(page);
 
