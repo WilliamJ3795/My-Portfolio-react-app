c0a364f3cfc2a7a41dbf4fa2feb40a32289b0407
William James <Williamj3795@gmail.com>
2023-01-18 21:52:31 -0500

    removed contact and testimonials sections

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillLaptopFill } from "react-icons/bs";
 
 export default function About() {
   return (
@@ -6,14 +7,17 @@ export default function About() {
     <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start 
                        md:text-left mb-16 md:mb-0 items-center text-center">
+
+        <BsFillLaptopFill className="object-cover h-48 w-96" />
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
           Hi! My name is William,
           <br className="hidden lg:inline-block" /> I love web development!
         </h1>
         <p className="mb-8 leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-          laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-          Laborum, voluptas natus?
+         My fascination with tech dates back to when I was just a child, 
+         but it wasn't until 2022 where I decided to begin my journey into full stack web development.
+         Feel free to navigate my portfolio!  
+
         </p>
         {/* <div className="flex justify-center">
           <a
