3ed4b9b45163a19ea73042bbc9b1dd6b23391424
William James <Williamj3795@gmail.com>
2023-01-18 22:52:12 -0500

    changed some syntax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import React from "react";
-
-// import About from "./components/About";
-// import Contact from "./components/Contact";
-// import Navbar from "./components/Navbar";
-// import Projects from "./components/Projects";
-// import Skills from "./components/Skills";
-// import Testimonials from "./components/Testimonials";
-
+import PortfolioContainer from "./components/PortfolioContainer";
 
 const App = () => <PortfolioContainer />;
 
 export default App;
-
-// export default function App() {
-//   return (
-//     <main className="text-gray-400 bg-gray-900 body-font">
-//       <Navbar />
-//       <About />
-//       <Projects />
-//       <Skills />
-//       <Testimonials />
-//       <Contact />
-//     </main>
-//   );
-// }
